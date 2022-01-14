83837def86e661b6c62a01ac9f22de4aac2cece6
Commissar94 <andreykomissarov94@gmail.com>
2022-01-14 09:00:16 +0300

    + new

---
+++ src/kyu8/ConvertANumberToAString.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class ConvertANumberToAString {
+    public static void main(String[] args) {
+
+    }
+
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
