ec009e4541d00a1a5c372858abdb54baa5c6a49e
Commissar94 <andreykomissarov94@gmail.com>
2022-01-16 19:54:04 +0300

    + new

---
+++ src/kyu8/ConvertBooleanValuesToStrings.java
@@ -0,0 +1,11 @@
+package kyu8;
+
+public class ConvertBooleanValuesToStrings {
+    public static void main(String[] args) {
+
+    }
+
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+}
